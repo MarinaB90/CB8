6861fa8589c7aabcddc7a1b69aa0ffb234e12f6f
MarinaB90 <148492258+MarinaB90@users.noreply.github.com>
2023-11-01 17:01:11 +0100

    Add files via upload

---
+++ 01-11-23/script.js
@@ -0,0 +1,8 @@
+// console.log(2);
+//console.log(2 + 5);
+
+const animalNames = ["dog", "cat", "rabbit", "mouse", "bear", "bee", "whale"];
+
+for (let i = 0; i < animalNames.length; i++) {
+  console.log(i + ":" + animalNames[i]);
+}
